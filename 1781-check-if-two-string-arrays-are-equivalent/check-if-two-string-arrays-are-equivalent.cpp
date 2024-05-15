b0ccc032c8b91f35f23e8389d52d7a281838860c
Sivasaikrishna19 <146486981+Sivasaikrishna19@users.noreply.github.com>
2024-05-15 15:32:47 -0700

    Time: 4 ms (63.97%) | Memory: 13.2 MB (76.88%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1="";
-        string s2="";
-        for(int i=0;i<word1.size();i++){
-            s1 +=word1[i];
+         int i=0,j=0; //outer pointers
+        int in_i=0,in_j=0; //internal pointers
+        
+        while(i<word1.size() && j<word2.size()){
+            string s1 = word1[i];
+            string s2 = word2[j];
+            while(in_i < s1.length() && in_j <s2.length()){
+                if(s1[in_i]!=s2[in_j]){
+                    return false;
+                }
+                in_i++;
+                in_j++;
+            }
+             if (in_i == s1.length()) {
+                i++;
+                in_i = 0;
+            }
+            if (in_j == s2.length()) {
+                j++;
+                in_j = 0;
+            }
         }
-        for(int i=0;i<word2.size();i++){
-            s2 +=word2[i];
+        if(i != word1.size() || j != word2.size()){
+            return false;
         }
-
-        return s1==s2;
+        return true;
+    
     }
 };
