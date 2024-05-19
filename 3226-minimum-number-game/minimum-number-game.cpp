517aceb4137071ac521b02dbe3055940be4f3fca
Sivasaikrishna19 <146486981+Sivasaikrishna19@users.noreply.github.com>
2024-05-19 09:42:29 -0700

    Time: 7 ms (64.21%) | Memory: 27.4 MB (34.20%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()-1){
+            ans.push_back(nums[i+1]);
+            ans.push_back(nums[i]);
+            i +=2;
+        }
+
+        return ans;
+    }
+};
